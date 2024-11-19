f83801d61c0b71ad3d793b686ca880c3a47c6c89
Kim Seunjae <rlatmdwo9@gmail.com>
2024-11-19 18:07:42 +0900

    props & type

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { Test } from './Test';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-function App() {
+export const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Test name = "Test"/>
+      <Test/>
+      <Codelab name="Otter"> baby otter </Codelab>
     </div>
   );
-}
-
+};
 export default App;
+
+
+export const Codelab =(props) =>{
+  return(
+    <div>
+      <h1>{props.name}</h1>
+      <h1>{props.children}</h1>
+    </div>
+  );
+};
+
+// PropTypes 설정
+Codelab.propTypes = {
+  name: PropTypes.string.isRequired, // name은 문자열이며 필수
+  children: PropTypes.node           // children은 React 노드 (문자열, 요소 등)
+};
+
+// 기본값 설정
+Codelab.defaultProps = {
+  name: 'Default Name', // name의 기본값
+};
