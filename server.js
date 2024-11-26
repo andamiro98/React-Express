47891aa4f39afd3c274ee97e795f3ba6fc1dcd65
Kim Seunjae <rlatmdwo9@gmail.com>
2024-11-26 15:18:11 +0900

    Files upload

--- server.js
+++ server.js
@@ -14,3 +14,21 @@ app.get('/', (req, res) => {
    res.sendFile(path.join(__dirname, '/react-project/build/index.html'));
 });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname, '/react-project/build/index.html'));
+});
